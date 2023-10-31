f87b90b6a8f874f77fc2d3fd5243246ea964f328
Jero1304 <calogeroleto13@gmail.com>
2023-10-31 10:39:41 +0100

    ADD about & children image-slider-side components

---
+++ src/app/componenti/about/image-slider-side/image-slider-side.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-slider-side',
+  templateUrl: './image-slider-side.component.html',
+  styleUrls: ['./image-slider-side.component.sass']
+})
+export class ImageSliderSideComponent {
+
+}
