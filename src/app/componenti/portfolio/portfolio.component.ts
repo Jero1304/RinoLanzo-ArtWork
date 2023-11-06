69f33813354034c4d780b5e7e05df82ea419e35d
Jero1304 <calogeroleto13@gmail.com>
2023-11-06 15:25:57 +0100

    ADD pagination short

--- src/app/componenti/portfolio/portfolio.component.ts
+++ src/app/componenti/portfolio/portfolio.component.ts
@@ -13,6 +13,7 @@ export class PortfolioComponent {
   chunkRange = 6;
   operaDefault = false;
   selectedOpera: any = null;
+  paginatorIndex = 0;
 
   constructor(private imageService: ImageService) {}
 
@@ -27,34 +28,67 @@ export class PortfolioComponent {
       // console.log(this.images.slice(i, i + chunkSize));
       results.push(this.images.slice(i, i + chunkSize));
     }
-    console.log(results);
+    // console.log(results);
     return results;
   }
 
   nextSlide() {
     this.currentIndex++;
-      if (this.currentIndex >= this.chunkedImages.length) {
-        this.currentIndex = 0;
-      }
-      this.chunkedImages = this.chunkArray(this.chunkRange);
+    if (this.currentIndex >= this.chunkedImages.length) {
+      this.currentIndex = 0;
+    }
+    this.chunkedImages = this.chunkArray(this.chunkRange);
   }
 
   previusSlide() {
     this.currentIndex--;
-      if (this.currentIndex < 0) {
-        this.currentIndex = this.chunkedImages.length - 1;
-      }
-      this.chunkedImages = this.chunkArray(this.chunkRange);
+    if (this.currentIndex < 0) {
+      this.currentIndex = this.chunkedImages.length - 1;
+    }
+    this.chunkedImages = this.chunkArray(this.chunkRange);
   }
 
   currentOpera(imageChunk: any) {
     const operaPath = imageChunk.path;
     this.imageService.setSelectedOpera(operaPath);
     this.operaDefault = true;
-    this.selectedOpera = operaPath
+    this.selectedOpera = operaPath;
   }
 
-  changePage(currentPage:number){
-    this.currentIndex = currentPage
+  changePage(currentPage: number) {
+    this.currentIndex = currentPage;
+  }
+
+  changePaginatorPage(paginatorIndex: number) {
+    this.paginatorIndex = paginatorIndex;
+    console.log(this.paginatorIndex);
+  }
+
+  paginatorSlice() {
+
+    return this.chunkArray(this.chunkRange).slice(
+      this.paginatorIndex,
+      this.paginatorIndex + 5
+    );
+  }
+
+  nextPaginator() {
+    console.log(this.paginatorSlice().length);
+    if (this.paginatorIndex + 5 < this.chunkArray(this.chunkRange).length) {
+      this.paginatorIndex += 5;
+    } else {
+      this.paginatorIndex = 0;
+    }
+    this.changePaginatorPage(this.paginatorIndex);
+  }
+
+  previusPaginator() {
+    if (this.paginatorIndex > 0) {
+      this.paginatorIndex -= 5;
+    } else {
+      this.paginatorIndex = 0;
+      console.log(this.paginatorIndex);
+    }
+    this.changePaginatorPage(this.paginatorIndex);
   }
 }
