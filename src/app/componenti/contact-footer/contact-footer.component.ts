4dc87f7b91622b46f92c6a3fefabad851f5c1a71
Jero1304 <calogeroleto13@gmail.com>
2023-11-09 15:09:11 +0100

    ADD footer animation

--- src/app/componenti/contact-footer/contact-footer.component.ts
+++ src/app/componenti/contact-footer/contact-footer.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact-footer',
   templateUrl: './contact-footer.component.html',
-  styleUrls: ['./contact-footer.component.sass']
+  styleUrls: ['./contact-footer.component.sass'],
 })
 export class ContactFooterComponent {
+  mail: boolean = false;
+  instagram: boolean = false;
+  location: boolean = false;
 
+  currentItem(itemName: string) {
+    this.mail = itemName === 'mail';
+    this.instagram = itemName === 'instagram';
+    this.location = itemName === 'location';
+  }
+  
 }
